6514a7acba94b237a85066315f344b770dc6b3bd
alisayed987 <ali.sayed987@yahoo.com>
2023-11-16 01:03:35 +0200

    migrate from vite to laravel mix

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Transactions App') }}</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
+</head>
+<body>
+    <div>Content</div>
+</body>
+
+</html>
