88ffa9daea30681e0fe5175cb66fd567c5913b80
alisayed987 <ali.sayed987@yahoo.com>
2023-11-16 01:33:45 +0200

    add tailwind to the project

--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -14,5 +14,5 @@ const mix = require('laravel-mix');
 mix.setPublicPath('public')
     .js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss")
     ]);
