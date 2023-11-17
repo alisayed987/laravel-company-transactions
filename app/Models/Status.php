5491f1c7de70653ef1fd1b7c18d7c0c74413c26d
alisayed987 <ali.sayed987@yahoo.com>
2023-11-17 20:19:05 +0200

    transactions and statuses model

---
+++ app/Models/Status.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Status extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $fillable = [
+        'name',
+    ];
+
+    public function transactions()
+    {
+        return $this->belongsToMany(Transaction::class);
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    public $fillable = [
+        'amount',
+        'payer',
+        'due_on',
+        'VAT',
+        'is_VAT_inclusive',
+    ];
+
+    public function statuses()
+    {
+        return $this->belongsToMany(Status::class);
+    }
+}
